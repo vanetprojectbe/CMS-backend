e308d9c94e1af23485af1ea55c83c3c920761c97
vanetprojectbe <vanetprojectbe@gmail.com>
2026-03-07 22:01:52 +0530

    Update server.js

--- vanet-cms-backend/src/server.js
+++ vanet-cms-backend/src/server.js
@@ -35,3 +35,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.get("/api/test", (req, res) => {
+  res.json({ message: "API working" });
+});
